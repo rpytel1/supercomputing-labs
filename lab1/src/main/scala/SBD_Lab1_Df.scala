1163c91df17c12f6a56838c7af29248d6b29ea2d
Vasilis Serentellos <seredellos@gmail.com>
2019-09-12 19:03:10 +0200

    fixing formatting for Github

--- lab1/src/main/scala/SBD_Lab1_Df.scala
+++ lab1/src/main/scala/SBD_Lab1_Df.scala
@@ -11,118 +11,118 @@ import scala.collection.mutable.WrappedArray
 
 
 object SBD_Lab1_Df {
-    case class GdeltData (				// the class of the Gdelt dataset
-	    GKGRECORDID: String,
-	    DATE: Date,
-	    SourceCollectionIdentifier: Int,
-	    SourceCommonName: String,
-	    DocumentIdentifier: String,
-	    Counts: String,
-	    V2Counts: String,
-	    Themes: String,
-	    V2Themes: String,
-	    Locations: String,
-	    V2Locations: String,
-	    Persons: String,
-	    V2Persons: String,
-	    Organizations: String,
-	    V2Organizations: String,
-	    V2Tone: String,
-	    Dates: String,
-	    GCAM: String,
-	    SharingImage: String,
-	    RelatedImages: String,
-	    SocialImageEmbeds: String,
-	    SocialVideoEmbeds: String,
-	    Quotations: String,
-	    AllNames: String,
-	    Amounts: String,
-	    TranslationInfo: String,
-	    Extras: String
-	)
+    case class GdeltData (                  // the class of the Gdelt dataset
+        GKGRECORDID: String,
+        DATE: Date,
+        SourceCollectionIdentifier: Int,
+        SourceCommonName: String,
+        DocumentIdentifier: String,
+        Counts: String,
+        V2Counts: String,
+        Themes: String,
+        V2Themes: String,
+        Locations: String,
+        V2Locations: String,
+        Persons: String,
+        V2Persons: String,
+        Organizations: String,
+        V2Organizations: String,
+        V2Tone: String,
+        Dates: String,
+        GCAM: String,
+        SharingImage: String,
+        RelatedImages: String,
+        SocialImageEmbeds: String,
+        SocialVideoEmbeds: String,
+        Quotations: String,
+        AllNames: String,
+        Amounts: String,
+        TranslationInfo: String,
+        Extras: String
+    )
 
-	def main(args: Array[String]) {
+    def main(args: Array[String]) {
 
-		val schema = StructType(										// the schema of the Gdelt dataset
-			Array(
-			    StructField("GKGRECORDID", StringType, nullable = true),
-			    StructField("DATE", DateType, nullable = true),
-			    StructField("SourceCollectionIdentifier", IntegerType, nullable = true),
-			    StructField("SourceCommonName", StringType, nullable = true),
-			    StructField("DocumentIdentifier", StringType, nullable = true),
-			    StructField("Counts", StringType, nullable = true),
-			    StructField("V2Counts", StringType, nullable = true),
-			    StructField("Themes", StringType, nullable = true),
-			    StructField("V2Themes", StringType, nullable = true),
-			    StructField("Locations",StringType, nullable = true),
-			    StructField("V2Locations",StringType, nullable = true),
-			    StructField("Persons", StringType, nullable = true),
-			    StructField("V2Persons", StringType, nullable = true),
-			    StructField("Organizations", StringType, nullable = true),
-			    StructField("V2Organizations", StringType, nullable = true),
-			    StructField("V2Tone", StringType, nullable = true),
-			    StructField("Dates",StringType, nullable = true),
-			    StructField("GCAM", StringType, nullable = true),
-			    StructField("SharingImage", StringType, nullable = true),
-			    StructField("RelatedImages", StringType, nullable = true),
-			    StructField("SocialImageEmbeds", StringType, nullable = true),
-			    StructField("SocialVideoEmbeds", StringType, nullable = true),
-			    StructField("Quotations", StringType, nullable = true),
-			    StructField("AllNames", StringType, nullable = true),
-			    StructField("Amounts", StringType, nullable = true),
-			    StructField("TranslationInfo", StringType, nullable = true),
-			    StructField("Extras", StringType, nullable = true)
-			)
-		)
+        val schema = StructType(              // the schema of the Gdelt dataset
+            Array(
+                StructField("GKGRECORDID", StringType, nullable = true),
+                StructField("DATE", DateType, nullable = true),
+                StructField("SourceCollectionIdentifier", IntegerType, nullable = true),
+                StructField("SourceCommonName", StringType, nullable = true),
+                StructField("DocumentIdentifier", StringType, nullable = true),
+                StructField("Counts", StringType, nullable = true),
+                StructField("V2Counts", StringType, nullable = true),
+                StructField("Themes", StringType, nullable = true),
+                StructField("V2Themes", StringType, nullable = true),
+                StructField("Locations",StringType, nullable = true),
+                StructField("V2Locations",StringType, nullable = true),
+                StructField("Persons", StringType, nullable = true),
+                StructField("V2Persons", StringType, nullable = true),
+                StructField("Organizations", StringType, nullable = true),
+                StructField("V2Organizations", StringType, nullable = true),
+                StructField("V2Tone", StringType, nullable = true),
+                StructField("Dates",StringType, nullable = true),
+                StructField("GCAM", StringType, nullable = true),
+                StructField("SharingImage", StringType, nullable = true),
+                StructField("RelatedImages", StringType, nullable = true),
+                StructField("SocialImageEmbeds", StringType, nullable = true),
+                StructField("SocialVideoEmbeds", StringType, nullable = true),
+                StructField("Quotations", StringType, nullable = true),
+                StructField("AllNames", StringType, nullable = true),
+                StructField("Amounts", StringType, nullable = true),
+                StructField("TranslationInfo", StringType, nullable = true),
+                StructField("Extras", StringType, nullable = true)
+            )
+        )
 
-		Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+        Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+
+        val spark = SparkSession.builder.appName("SBD_Lab1").config("spark.master", "local[*]").getOrCreate()
+
+        import spark.implicits._	
+
+        // a user-defined function for converting a WrappedArray column to Set and then back to Array
+        val mkSet = udf((arrayCol: Seq[String]) => arrayCol.asInstanceOf[WrappedArray[String]].toSet.toArray)
+
+        // a user-defined function for converting a list of lists to an array of struct of tuples
+        val mkList = udf((arrayCol: Seq[Seq[String]]) => 
+            arrayCol.asInstanceOf[WrappedArray[WrappedArray[String]]].map(s => s.toString.replaceAll("[()]", "").replaceAll("\\bWrappedArray","").split(",\\s")).map { case Array(f1,f2) => (f1,f2.toInt)}) 
 		
-		val spark = SparkSession.builder.appName("SBD_Lab1").config("spark.master", "local[*]").getOrCreate()
+        //the final JSON schema 
+        val finalJSONSchema: String = "array<struct<topic:string,count:bigint>>"
 
-		import spark.implicits._	
-		
-		// a user-defined function for converting a WrappedArray column to Set and then back to Array
-		val mkSet = udf((arrayCol: Seq[String]) => arrayCol.asInstanceOf[WrappedArray[String]].toSet.toArray)
-		
-		// a user-defined function for converting a list of lists to an array of struct of tuples
-		val mkList = udf((arrayCol: Seq[Seq[String]]) => 
-			arrayCol.asInstanceOf[WrappedArray[WrappedArray[String]]].map(s => s.toString.replaceAll("[()]", "").replaceAll("\\bWrappedArray","").split(",\\s")).map { case Array(f1,f2) => (f1,f2.toInt)}) 
-		
-		//the final JSON schema 
-		val finalJSONSchema: String = "array<struct<topic:string,count:bigint>>"
+        val ds = spark.read.format("csv")
+                .option("delimiter", "\t")              // set the delimeter option as tab
+                .option("dateFormat", "yyyyMMddHHmmss") // set the date format
+                .schema(schema).csv("data/seg10.csv").as[GdeltData]
 
-		val ds = spark.read.format("csv")
-				.option("delimiter", "\t")				// set the delimeter option as tab
-				.option("dateFormat", "yyyyMMddHHmmss")	// set the date format
-				.schema(schema).csv("data/seg10.csv").as[GdeltData]
+        val processed_ds = ds
+                            .filter(col("DATE").isNotNull && col("AllNames").isNotNull)                         // filter out the null entries
+                            .select("DATE", "AllNames")                                                         // keep only the DATE and AllNames columns
+                            .withColumn("AllNames", regexp_replace($"AllNames" ,"[,0-9]", ""))                  // keep only the name of the entities 
+                            .withColumn("OnlyNames", split($"AllNames", ";"))                                   // split these names 
+                            .drop("AllNames")                                                                   // drop the unnecessary column
+                            .withColumn("DistinctNames", mkSet($"OnlyNames"))                                   // keep only one occurence for each name in each file
+                            .drop("OnlyNames")                                                                  // drop the unnecessary column
+                            .withColumn("DistinctNames", explode($"DistinctNames"))                             // convert column to rows so that all date - name pairs are created
+                            .filter(!col("DistinctNames").contains("Type ParentCategory"))                      // Filter out ParentCategory
+                            .groupBy("DATE", "DistinctNames")                                                   // group by the columns in order to count the occurences
+                            .count                                                                              // of each distinct name in each day
+                            .withColumn("Rank", rank.over(Window.partitionBy("DATE").orderBy($"count".desc)))   // partition by date and find the rank in each day window
+                            .filter(col("Rank") <= 10)                                                          //  keep only the top 10 counts for each day
+                            .drop("Rank")                                                                       // drop the rank column
+                            .withColumn("merged", array("DistinctNames", "count"))                              // and merge the count and names in a tuple
+                            .drop("DistinctNames")                                                              // drop the unnecessary columns
+                            .drop("count")
+                            .groupBy("DATE")                                                                    // and finally group by DATE
+                            .agg(collect_list(col("merged")).as("TopNames"))					
+                            .orderBy($"DATE".asc)                                                               // and ascendingly order by date
+                            .withColumn("TopNames", mkList($"TopNames"))                                        // change the structure of the final dataset
+                            .select('DATE as "data", 'TopNames.cast(finalJSONSchema) as "result")               // and apply the final JSON format
+                            .toJSON
 
-		val processed_ds = ds
-						.filter(col("DATE").isNotNull && col("AllNames").isNotNull)							// filter out the null entries
-						.select("DATE", "AllNames")															// keep only the DATE and AllNames columns
-						.withColumn("AllNames", regexp_replace($"AllNames" ,"[,0-9]", ""))					// keep only the name of the entities 
-						.withColumn("OnlyNames", split($"AllNames", ";"))									// split these names 
-						.drop("AllNames")																	// drop the unnecessary column
-						.withColumn("DistinctNames", mkSet($"OnlyNames")) 									// keep only one occurence for each name in each file
-						.drop("OnlyNames")																	// drop the unnecessary column
-						.withColumn("DistinctNames", explode($"DistinctNames"))								// convert column to rows so that all date - name pairs are created
-						.filter(!col("DistinctNames").contains("Type ParentCategory"))						// Filter out ParentCategory
-						.groupBy("DATE", "DistinctNames")													// group by the columns in order to count the occurences
-						.count 																				// of each distinct name in each day
-						.withColumn("Rank", rank.over(Window.partitionBy("DATE").orderBy($"count".desc)))	// partition by date and find the rank in each day window
-						.filter(col("Rank") <= 10)															//  keep only the top 10 counts for each day
-						.drop("Rank")																		// drop the rank column
-						.withColumn("merged", array("DistinctNames", "count"))								// and merge the count and names in a tuple
-						.drop("DistinctNames")																// drop the unnecessary columns
-						.drop("count")
-						.groupBy("DATE")																	// and finally group by DATE
-						.agg(collect_list(col("merged")).as("TopNames"))					
-						.orderBy($"DATE".asc)																// and ascendingly order by date
-						.withColumn("TopNames", mkList($"TopNames"))										// change the structure of the final dataset
-						.select('DATE as "data", 'TopNames.cast(finalJSONSchema) as "result")				// and apply the final JSON format
-						.toJSON
+        processed_ds.collect().foreach(println)     // print the wanted result
 
-		processed_ds.collect().foreach(println)		// print the wanted result
-		
-		spark.stop()
-	}
+        spark.stop()
+    }
 }
