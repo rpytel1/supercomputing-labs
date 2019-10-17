1c5005ec767989fb683112d396d3a72c8421971d
Vasilis Serentellos <seredellos@gmail.com>
2019-10-17 14:27:50 +0200

    semi working stream

--- lab3/GDELTStream/GDELTStream.scala
+++ lab3/GDELTStream/GDELTStream.scala
@@ -8,7 +8,8 @@ import org.apache.kafka.streams.processor._
 import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala._
 import org.apache.kafka.streams.scala.kstream._
-import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig, KeyValue}
+import org.apache.kafka.streams.state.{KeyValueStore, Stores}
 
 import scala.collection.JavaConversions._
 
@@ -24,10 +25,26 @@ object GDELTStream extends App {
 
   val builder: StreamsBuilder = new StreamsBuilder
 
+  val countStoreSupplier = Stores
+    .keyValueStoreBuilder(
+      Stores.persistentKeyValueStore("Counts-last-hour"), 
+      Serdes.String, 
+      Serdes.Long
+    )
+    .withLoggingDisabled()
+  
+  builder.addStateStore(countStoreSupplier)
+
   // Filter this stream to a stream of (key, name). This is similar to Lab 1,
   // only without dates! After this apply the HistogramTransformer. Finally,
   // write the result to a new topic called gdelt-histogram.
   val records: KStream[String, String] = builder.stream[String, String]("gdelt")
+
+  records.mapValues(value => value.split("\t"))
+         .filter((_, value) => value.length > 23)
+         .flatMapValues(value => value(23).replaceAll("[,0-9]", "").split(";").filter(_ != ""))
+         .transform[String, Long](() => new HistogramTransformer(), "Counts-last-hour")
+         .to("gdelt-histogram")
 
   val streams: KafkaStreams = new KafkaStreams(builder.build(), props)
   streams.cleanUp()
@@ -46,19 +63,49 @@ object GDELTStream extends App {
 //  2. Change the count for a record if it occurs again; and
 //  3. Decrement the count of a record an hour later.
 // You should implement the Histogram using a StateStore (see manual)
-class HistogramTransformer extends Transformer[String, String, (String, Long)] {
+class HistogramTransformer extends Transformer[String, String, KeyValue[String, Long]] {
   var context: ProcessorContext = _
+  var kvStore: KeyValueStore[String, Long] = _
 
   // Initialize Transformer object
-  def init(context: ProcessorContext) {
+  override def init(context: ProcessorContext): Unit = {
     this.context = context
+    // retrieve the key-value store named "Counts-last-hour"
+    this.kvStore = context.getStateStore("Counts-last-hour").asInstanceOf[KeyValueStore[String, Long]]
   }
 
   // Should return the current count of the name during the _last_ hour
-  def transform(key: String, name: String): (String, Long) = {
-    ("Donald Trump", 1L)
+  override def transform(key: String, name: String): KeyValue[String, Long] = {
+    // calculate the time passed for decrementing the count
+    // val currentTime: Long = System.currentTimeMillis()
+    // val recordTime: Long = this.context.timestamp()
+    // val retentionTime: Long = TimeUnit.MILLISECONDS.convert(1L, TimeUnit.HOURS)
+    // val interval = retentionTime - (currentTime - recordTime)
+    val interval: Long = TimeUnit.MILLISECONDS.convert(20L, TimeUnit.SECONDS)
+
+    // schedule the decrementing of the count after 1 hour
+    var scheduledDecrease: Cancellable = null
+    scheduledDecrease = this.context.schedule(interval, PunctuationType.WALL_CLOCK_TIME, timestamp => {
+
+      // decrement by 1
+      val newCount = kvStore.get(name) - 1L
+      kvStore.put(name, newCount)
+      context.forward(name, newCount)
+
+      // cancel this schedule, so it runs only once
+      scheduledDecrease.cancel()
+    })
+    // add new record to StateStore
+    this.kvStore.putIfAbsent(name, 0L)
+
+    // increment the record's count by 1
+    val newCount = this.kvStore.get(name) + 1L
+    this.kvStore.put(name, newCount)
+
+    // and finally return the count of the record over the last hour
+    (name, newCount)
   }
 
   // Close any resources if any
-  def close() {}
+  override def close(): Unit = {}
 }
