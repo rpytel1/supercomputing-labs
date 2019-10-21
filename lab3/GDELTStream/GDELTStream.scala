4d7a1a931a5d09519b019d93b9eab7255562db6e
Vasilis Serentellos <seredellos@gmail.com>
2019-10-21 17:59:39 +0200

    fully working stream implementation

--- lab3/GDELTStream/GDELTStream.scala
+++ lab3/GDELTStream/GDELTStream.scala
@@ -8,7 +8,7 @@ import org.apache.kafka.streams.processor._
 import org.apache.kafka.streams.scala.ImplicitConversions._
 import org.apache.kafka.streams.scala._
 import org.apache.kafka.streams.scala.kstream._
-import org.apache.kafka.streams.{KafkaStreams, StreamsConfig, KeyValue, Topology} // to check topology
+import org.apache.kafka.streams.{KafkaStreams, StreamsConfig, KeyValue} 
 import org.apache.kafka.streams.state.{KeyValueIterator, KeyValueStore, Stores}
 
 import scala.collection.JavaConversions._
@@ -26,19 +26,20 @@ object GDELTStream extends App {
 
   val builder: StreamsBuilder = new StreamsBuilder
 
+  // first create the stateStore for counting the number of occurences per record name
   val countStoreSupplier = Stores
     .keyValueStoreBuilder(
       Stores.persistentKeyValueStore("Counts-last-hour"), 
       Serdes.String, 
       Serdes.Long
     )
-    // .withLoggingDisabled()
 
+  // and then create the stateStore for the timestamps
   val timeStoreSupplier = Stores
     .keyValueStoreBuilder(
       Stores.persistentKeyValueStore("Timestamps-last-hour"), 
       Serdes.String, 
-      Serdes.String
+      Serdes.Long
     )
   
   builder.addStateStore(countStoreSupplier)
@@ -49,22 +50,13 @@ object GDELTStream extends App {
   // write the result to a new topic called gdelt-histogram.
   val records: KStream[String, String] = builder.stream[String, String]("gdelt")
 
-  // val wordPattern = "\\p{L}".r
+  records.mapValues(value => value.split("\t"))     // first split the records by tabs
+         .filter((_, value) => value.length > 23)   // then keep only those with an AllNames column
+         .flatMapValues(value => value(23).replaceAll("[,0-9]", "").split(";"))   // and remove commas and numbers and split by ; to retrieve each record in an article
+         .filter((_, value) => !value.trim.equals(""))    // remove all records that after trimming are empty
+         .transform[String, Long](() => new HistogramTransformer(), "Counts-last-hour", "Timestamps-last-hour")   // and apply the HistogramTransformer
+         .to("gdelt-histogram")  
 
-  records.mapValues(value => value.split("\t"))
-         .filter((_, value) => value.length > 23)
-         .flatMapValues(value => value(23).replaceAll("[,0-9]", "").split(";"))
-         .mapValues(value => value.trim)
-         // .mapValues(value => value.replaceAll("\n", "").replaceAll("""(?m)\s+$""", "").trim) // maybe to be removed
-         .filter((_, value) => !value.trim.equals(""))
-         // .filter((_, value) => wordPattern.findFirstIn(value)!= None)
-         // .filter((_, value) => wordPattern.findFirstIn(value)!= None && !value.trim.equals("\\P{Other}") && !value.trim.equals("\\P{Z}"))
-         .transform[String, Long](() => new HistogramTransformer(), "Counts-last-hour", "Timestamps-last-hour")
-         .to("gdelt-histogram")
-
-  
-  // val topology: Topology = builder.build()  // just to check topology
-  // println(topology.describe)                // the created topology
   val streams: KafkaStreams = new KafkaStreams(builder.build() , props)
   streams.cleanUp()
   streams.start()
@@ -85,32 +77,35 @@ object GDELTStream extends App {
 class HistogramTransformer extends Transformer[String, String, KeyValue[String, Long]] {
   var context: ProcessorContext = _
   var kvStore: KeyValueStore[String, Long] = _
-  var timeStore: KeyValueStore[String, String] = _
+  var timeStore: KeyValueStore[String, Long] = _
 
   // Initialize Transformer object
   override def init(context: ProcessorContext): Unit = {
     this.context = context
     // retrieve the key-value store named "Counts-last-hour"
     this.kvStore = context.getStateStore("Counts-last-hour").asInstanceOf[KeyValueStore[String, Long]]
-    this.timeStore = context.getStateStore("Timestamps-last-hour").asInstanceOf[KeyValueStore[String, String]]
+    // retrieve the key-value store named "Timestamps-last-hour"
+    this.timeStore = context.getStateStore("Timestamps-last-hour").asInstanceOf[KeyValueStore[String, Long]]
 
+    // initiate the scheduler to be invoked every 500 miliseconds od stream time to check if a record is older than an hour
     this.context.schedule(500, PunctuationType.STREAM_TIME, timestamp => {
 
-      val iterator: KeyValueIterator[String, String] = timeStore.all()
+      val iterator: KeyValueIterator[String, Long] = timeStore.all()  // take an iterator of the timestamp stateStore
+      // val interval: Long = TimeUnit.MILLISECONDS.convert(1L, TimeUnit.HOURS)
 
       while (iterator.hasNext) {
 
-        val e: KeyValue[String, String] = iterator.next()
+        val e: KeyValue[String, Long] = iterator.next()   
+        val recordTime: Long = e.key.split('|')(0).toLong   // take the record's timestamp
+        val record: String = e.key.split('|')(1)            // take the record's name
 
-        // If it was more than an hour has passed
-        if (((timestamp - e.key.toLong) / 20000) > 1) {
-          //3. Decrement the count of a record an hour later.
-          kvStore.put(e.value, kvStore.get(e.value) - 1L)
-          //Delete it from the log
-          timeStore.delete(e.key)
+        // If it was more than 30s have passed -> to be replaced by interval for the final implemntation
+        if (((timestamp - recordTime) / 30000) > 1) {
+          kvStore.put(record, kvStore.get(record) - e.value)  // decrement the count of the record by the count of the timestamp-record pair
+          timeStore.delete(e.key) // and delete the timestamp-record entry from the timestamp statestore
         }
-        //Output of a name with decremented count based on time
-        context.forward(e.value, kvStore.get(e.value))
+        // and forward the decreased key-value pair to the downstream processor 
+        context.forward(record, kvStore.get(record))
       }
       context.commit()
     })
@@ -118,35 +113,24 @@ class HistogramTransformer extends Transformer[String, String, KeyValue[String, 
 
   // Should return the current count of the name during the _last_ hour
   override def transform(key: String, name: String): KeyValue[String, Long] = {
-    // set the interval to 1 hour (for testing reasons it is set to 20 secs)
-    // val interval: Long = TimeUnit.MILLISECONDS.convert(20L, TimeUnit.SECONDS)
+    // retrieve the timestamp of the record
     val recordTimestamp: Long = this.context.timestamp()
+    // and create the key of the timeStore by concatenating the record's timestamp and its name with a pipe in between (for uniqueness)
+    val recordTimeKey: String = recordTimestamp.toString + '|' + name
 
-    // schedule the decrementing of the count after 1 hour
-    // var scheduledDecrease: Cancellable = null
-
-    // // PunctuationType.STREAM_TIME -> for taking into account the attached timestamps 
-    // // PunctuationType.WALL_CLOCK_TIME -> for taking into account the real clock
-    // scheduledDecrease = this.context.schedule(interval, PunctuationType.STREAM_TIME, timestamp => {
-
-    //   // decrement by 1
-    //   val newCount = kvStore.get(name) - 1L
-    //   kvStore.put(name, newCount)
-    //   context.forward(name, newCount)
-
-    //   // cancel this schedule, so it runs only once
-    //   scheduledDecrease.cancel()
-    // })
-    // add new record to StateStore
+    // add a new record to the count and the timestamp stateStores
     this.kvStore.putIfAbsent(name, 0L)
+    this.timeStore.putIfAbsent(recordTimeKey, 0L)
 
-    this.timeStore.put(recordTimestamp.toString, name)
+    // increment the record's time-count by 1 (so that we know how many records with the same timestamp and name we had for the needs of the decreasing part)
+    val newTimeCount = this.timeStore.get(recordTimeKey) + 1L
+    this.timeStore.put(recordTimeKey, newTimeCount)
 
     // increment the record's count by 1
     val newCount = this.kvStore.get(name) + 1L
     this.kvStore.put(name, newCount)
 
-    // and finally return the count of the record over the last hour
+    // and finally return the updated count of the record 
     (name, newCount)
   }
 
